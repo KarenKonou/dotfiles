6c6e27505379244b21d07b8ae2b1a313f3a89b75
Karen Konou <karen@functional.solutions>
2025-05-17 17:05:34 +0200

    init

---
+++ nvim/lua/config/set.lua
@@ -0,0 +1,14 @@
+vim.opt.nu = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+vim.cmd [[
+  highlight Normal guibg=none
+  highlight NonText guibg=none
+  highlight Normal ctermbg=none
+  highlight NonText ctermbg=none
+]]
